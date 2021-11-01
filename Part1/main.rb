26d4a68e8a405d3142146b76c5508fb690cfca18
iSerEzhik <74928925+iSerEzhik@users.noreply.github.com>
2021-11-01 15:47:43 +0300

    Laba7 part1

--- Part1/main.rb
+++ Part1/main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def compare_files(path_to_file_f, path_to_file_g)
-  File.new(path_to_file_f).gets.chomp.chars.zip(File.new(path_to_file_g).gets.chomp.chars)
+  File.new(path_to_file_f, 'r').gets.chomp.chars.zip(File.new(path_to_file_g, 'r').gets.chomp.chars)
       .each_with_index.map do |x, i|
     i + 1 if x[0] != x[1]
   end.compact.shift
--- Part1/user.rb
+++ Part1/user.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 require './main'
-[STDIN,STDOUT].each { |io| io.set_encoding(Encoding.find(Encoding.locale_charmap),__ENCODING__) }
-name = gets.chomp
-puts "Привет, #{name}"
+FILE_F_PATH = './F.txt'
+FILE_G_PATH = './G.txt'
+
+file_f = File.new(FILE_F_PATH, 'w')
+file_g = File.new(FILE_G_PATH, 'w')
+puts 'Введите строчку файла F:'
+file_f.write(gets.chomp)
+puts 'Введите строчку файла G:'
+file_g.write(gets.chomp)
+file_f.close
+file_g.close
+puts "Первый различный символ под номером #{compare_files(FILE_F_PATH, FILE_G_PATH)}"
