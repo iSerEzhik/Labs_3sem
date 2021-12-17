650d4190cf42a1e05113f01464b0662cb16fd1a6
iSerEzhik <74928925+iserezhik@users.noreply.github.com>
2021-12-17 12:19:32 +0300

    Laba12 Tests
    Signed-off-by: iSerEzhik <74928925+iserezhik@users.noreply.github.com>

--- laba8/db/schema.rb
+++ laba8/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_210843) do
+ActiveRecord::Schema.define(version: 2021_12_16_231927) do
+
+  create_table "laba8_models", force: :cascade do |t|
+    t.integer "number"
+    t.float "result"
+    t.string "iterates"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
